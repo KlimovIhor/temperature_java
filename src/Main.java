d3e594ba744357bc67009c8900d1377214a7d18b
kenshinwow1 <kenshinwow1@gmail.com>
2024-06-23 03:22:41 +0200

    rename

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
 Scanner scanner = new Scanner(System.in);
         System.out.println("Enter:");
-float pogoda = scanner.nextFloat();
+float weather = scanner.nextFloat();
 
-        if (pogoda < 0){
+        if (weather < 0){
             System.out.println("cold");
          }else{
             System.out.println("hot");
